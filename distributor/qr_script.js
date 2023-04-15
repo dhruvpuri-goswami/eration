f869ecdad4496010bb5d49280d5a3326b427b945
Dhruvpuri Goswami <92724441+Dhruvpuri-Goswami@users.noreply.github.com>
2023-04-15 22:30:09 +0530

    changes done

---
+++ distributor/qr_script.js
@@ -0,0 +1,6 @@
+function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code scanned = ${decodedText}`, decodedResult);
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+	"qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
